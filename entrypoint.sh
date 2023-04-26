689ba0dd7bcfb6e218ece9fad6920ac931c9978d
Elia Maggioni <elia.maggioni@gmail.com>
2023-04-26 22:28:47 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,10 @@
 # Start the server in the background
 /opt/cubecoders/amp/ampinstmgr StartInstance ADS01
 
-# Attach to the instance console
-/opt/cubecoders/amp/ampinstmgr View ADS01
+# Wait for the log file to be created
+while [ ! -f /home/amp/.ampdata/instances/ADS01/AMP_Logs/log.txt ]; do
+  sleep 1
+done
+
+# Tail the instance log file to keep the container running
+exec tail -f /home/amp/.ampdata/instances/ADS01/AMP_Logs/log.txt
