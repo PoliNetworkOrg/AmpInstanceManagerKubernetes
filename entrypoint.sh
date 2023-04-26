28435f4a05e73131144b2928c949098aec25fa12
Elia Maggioni <elia.maggioni@gmail.com>
2023-04-26 22:43:14 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@
 
 # Continuously view the instance output to keep the container running
 while true; do
-  /opt/cubecoders/amp/ampinstmgr View ADS01
+  exec tail -n 100 --follow=name /home/amp/.ampdata/instances/ADS01/AMP_Logs/AMPLOG*.log
   sleep 10
 done
