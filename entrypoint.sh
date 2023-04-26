915512b056f69c24d3731b33f6d4494579d264fb
Elia Maggioni <elia.maggioni@gmail.com>
2023-04-26 22:21:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,5 @@
 # Start the server in the background
 /opt/cubecoders/amp/ampinstmgr StartInstance ADS01
 
-# Sleep for a short period to ensure that the log file has been created
-sleep 5
-
-# Tail the instance's log file
-exec tail -f /home/amp/.ampdata/instances/ADS01/AMP_Logs/log.txt
+# Attach to the instance console
+/opt/cubecoders/amp/ampinstmgr View ADS01
