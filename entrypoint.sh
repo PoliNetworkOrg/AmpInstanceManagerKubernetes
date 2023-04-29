a19923d30c4e35462850254c45ad26d1749f8c15
Elia Maggioni <elia.maggioni@gmail.com>
2023-04-29 23:43:11 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,6 @@ fi
 
 # Continuously view the instance output to keep the container running
 while true; do
-  exec tail -n 100 --follow=name /home/amp/.ampdata/instances/ADS01/AMP_Logs/AMPLOG*.log
+  # exec tail -n 100 --follow=name /home/amp/.ampdata/instances/ADS01/AMP_Logs/AMPLOG*.log
   sleep 10
 done
