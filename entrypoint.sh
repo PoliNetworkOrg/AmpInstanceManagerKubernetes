d528244d0b1b46e1c6ec59ad923f6ae796679047
Elia Maggioni <elia.maggioni@gmail.com>
2023-04-29 23:42:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-/opt/cubecoders/amp/ampinstmgr --CreateInstance $MODULE ADS01 $IPBINDING $PORT $USERNAME $PASSWORD $LICENCE
+# /opt/cubecoders/amp/ampinstmgr --CreateInstance $MODULE ADS01 $IPBINDING $PORT $USERNAME $PASSWORD $LICENCE
 
 # Start the server in the background
-/opt/cubecoders/amp/ampinstmgr StartInstance ADS01
+# /opt/cubecoders/amp/ampinstmgr StartInstance ADS01
 
 # Continuously view the instance output to keep the container running
 while true; do
